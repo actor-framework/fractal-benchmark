dd752a1c0e209bd2194f2d64662538d00ad4ff04
neverlord <dominik.charousset@haw-hamburg.de>
2015-10-01 22:24:53 +0200

    Update scala run script

--- run-scala.sh
+++ run-scala.sh
@@ -1,19 +1,34 @@
-export AKKA_LIB=$AKKA_HOME/lib
-export SCALA_LIB=$SCALA_HOME/lib
+#!/bin/bash
+
+if [ -z "$AKKA_HOME" ] ; then
+  echo "AKKA_HOME is not set"
+  exit 1
+fi
+
+AKKA_LIB="$AKKA_HOME/lib"
 
 classpath=$(find $AKKA_HOME/lib/ -name "*.jar" | tr '\n' ':')
 
-export JVM_TUNING="-Xmx10240M -Xms32M"
-export SCALA_TUNING="-Xbootclasspath/a:$classpath -Dscala.home=$SCALA_HOME -Dscala.usejavacp=true"
+#export JAVA_HOME=$HOME/jdk1.8.0_60
+#export JAVA_CMD="$JAVA_HOME/bin/java"
+if [ -z "$JAVA_HOME" ] ; then
+  JAVA_CMD="$JAVA_HOME/bin/java"
+else
+  JAVA_CMD="java"
+fi
+JVM_TUNING="-Xmx10240M -Xms32M"
+SCALA_TUNING="-Xbootclasspath/a:$classpath -Dscala.usejavacp=true"
+AKKA_TUNING="-Dakka.remote.netty.tcp.hostname=$(ifconfig -a eth1 | grep "inet addr:" | awk '{print $2}' | sed 's/addr\://')"
 
+cd $HOME/fractal-benchmark/build/bin
 case "$1" in
     --master)
         echo "start master"
-        java $JVM_TUNING $SCALA_TUNING org.caf.distributed -n "akka.tcp://FractalWorkerSystem@127.0.0.1:2552/user/worker0, akka.tcp://FractalWorkerSystem@127.0.0.1:2552/user/worker1, akka.tcp://FractalWorkerSystem@127.0.0.1:2552/user/worker2, akka.tcp://FractalWorkerSystem@127.0.0.1:2552/user/worker3"
+        shift
+        $JAVA_CMD $JVM_TUNING $SCALA_TUNING $AKKA_TUNING org.caf.distributed -n $@
         ;;
     --worker)
-        echo "start worker"
-        java $JVM_TUNING $SCALA_TUNING -Djava.library.path=../ org.caf.distributed -w
+        echo "$JAVA_CMD $JVM_TUNING $SCALA_TUNING $AKKA_TUNING -Djava.library.path=../ org.caf.distributed -w"
         ;;
 esac
 
