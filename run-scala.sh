5e0570be4ca53c0795cc94482481c44433e65843
neverlord <dominik.charousset@haw-hamburg.de>
2015-10-01 22:31:31 +0200

    Read scala hostfile in run script

--- run-scala.sh
+++ run-scala.sh
@@ -12,9 +12,9 @@ classpath=$(find $AKKA_HOME/lib/ -name "*.jar" | tr '\n' ':')
 #export JAVA_HOME=$HOME/jdk1.8.0_60
 #export JAVA_CMD="$JAVA_HOME/bin/java"
 if [ -z "$JAVA_HOME" ] ; then
-  JAVA_CMD="$JAVA_HOME/bin/java"
-else
   JAVA_CMD="java"
+else
+  JAVA_CMD="$JAVA_HOME/bin/java"
 fi
 JVM_TUNING="-Xmx10240M -Xms32M"
 SCALA_TUNING="-Xbootclasspath/a:$classpath -Dscala.usejavacp=true"
@@ -23,12 +23,11 @@ AKKA_TUNING="-Dakka.remote.netty.tcp.hostname=$(ifconfig -a eth1 | grep "inet ad
 cd $HOME/fractal-benchmark/build/bin
 case "$1" in
     --master)
-        echo "start master"
-        shift
-        $JAVA_CMD $JVM_TUNING $SCALA_TUNING $AKKA_TUNING org.caf.distributed -n $@
+        slaves=$(cat "$2" | tr '\n' ',')
+        $JAVA_CMD $JVM_TUNING $SCALA_TUNING $AKKA_TUNING org.caf.distributed -n $slaves
         ;;
     --worker)
-        echo "$JAVA_CMD $JVM_TUNING $SCALA_TUNING $AKKA_TUNING -Djava.library.path=../ org.caf.distributed -w"
+        $JAVA_CMD $JVM_TUNING $SCALA_TUNING $AKKA_TUNING -Djava.library.path=../ org.caf.distributed -w
         ;;
 esac
 
